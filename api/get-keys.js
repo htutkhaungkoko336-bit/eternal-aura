83132e3f819e70450cd26e0592e4a3a001dc60fe
BarboSSA <htutkhaungkoko336@gmail.com>
2026-08-29 12:07:09 +0630

    355

--- api/get-keys.js
+++ api/get-keys.js
@@ -28,6 +28,7 @@ module.exports = async function handler(req, res) {
             return res.status(404).json({ success: false, message: "User not found" });
         }
 
+        // Database ထဲရှိ keys များကို Frontend မှ မျှော်လင့်ထားသော modes structure ထဲသို့ အသေအချာ Map လုပ်ခြင်း
         const userData = userDoc.data();
         const k = userData.keys || {}; // Database ထဲက keys object ကို ယူခြင်း
 
@@ -35,26 +36,25 @@ module.exports = async function handler(req, res) {
         const keys = {
             modes: {
                 '5v5': {
-                    '5k': k['5v5-5k'] !== undefined ? k['5v5-5k'] : (k['5v5_5k'] || 0),
-                    '10k': k['5v5-10k'] !== undefined ? k['5v5-10k'] : (k['5v5_10k'] || 0),
-                    '15k': k['5v5-15k'] !== undefined ? k['5v5-15k'] : (k['5v5_15k'] || 0),
-                    '25k': k['5v5-25k'] !== undefined ? k['5v5-25k'] : (k['5v5_25k'] || 0),
-                    '50k': k['5v5-50k'] !== undefined ? k['5v5-50k'] : (k['5v5_50k'] || 0)
+                    '5k': k['5v5-5k'] !== undefined ? k['5v5-5k'] : (k['5vs5-5k'] !== undefined ? k['5vs5-5k'] : (k['5v5_5k'] || k['5vs5_5k'] || 0)),
+                    '10k': k['5v5-10k'] !== undefined ? k['5v5-10k'] : (k['5vs5-10k'] !== undefined ? k['5vs5-10k'] : (k['5v5_10k'] || k['5vs5_10k'] || 0)),
+                    '15k': k['5v5-15k'] !== undefined ? k['5v5-15k'] : (k['5vs5-15k'] !== undefined ? k['5vs5-15k'] : (k['5v5_15k'] || k['5vs5_15k'] || 0)),
+                    '25k': k['5v5-25k'] !== undefined ? k['5v5-25k'] : (k['5vs5-25k'] !== undefined ? k['5vs5-25k'] : (k['5v5_25k'] || k['5vs5_25k'] || 0)),
+                    '50k': k['5v5-50k'] !== undefined ? k['5v5-50k'] : (k['5vs5-50k'] !== undefined ? k['5vs5-50k'] : (k['5v5_50k'] || k['5vs5_50k'] || 0))
                 },
                 '1v1': {
-                    '5k': k['1vs1-5k'] !== undefined ? k['1vs1-5k'] : (k['1vs1_5k'] || 0),
-                    '10k': k['1vs1-10k'] !== undefined ? k['1vs1-10k'] : (k['1vs1_10k'] || 0),
-                    '15k': k['1vs1-15k'] !== undefined ? k['1vs1-15k'] : (k['1vs1_15k'] || 0),
-                    '25k': k['1vs1-25k'] !== undefined ? k['1vs1-25k'] : (k['1vs1_25k'] || 0),
-                    '50k': k['1vs1-50k'] !== undefined ? k['1vs1-50k'] : (k['1vs1_50k'] || 0)
+                    '5k': k['1v1-5k'] !== undefined ? k['1v1-5k'] : (k['1vs1-5k'] !== undefined ? k['1vs1-5k'] : (k['1v1_5k'] || k['1vs1_5k'] || 0)),
+                    '10k': k['1v1-10k'] !== undefined ? k['1v1-10k'] : (k['1vs1-10k'] !== undefined ? k['1vs1-10k'] : (k['1v1_10k'] || k['1vs1_10k'] || 0)),
+                    '15k': k['1v1-15k'] !== undefined ? k['1v1-15k'] : (k['1vs1-15k'] !== undefined ? k['1vs1-15k'] : (k['1v1_15k'] || k['1vs1_15k'] || 0)),
+                    '25k': k['1v1-25k'] !== undefined ? k['1v1-25k'] : (k['1vs1-25k'] !== undefined ? k['1vs1-25k'] : (k['1v1_25k'] || k['1vs1_25k'] || 0)),
+                    '50k': k['1v1-50k'] !== undefined ? k['1v1-50k'] : (k['1vs1-50k'] !== undefined ? k['1vs1-50k'] : (k['1v1_50k'] || k['1vs1_50k'] || 0))
                 },
                 'tournament': {
                     'pass': k['tournament'] !== undefined ? k['tournament'] : (k['tour'] || 0)
                 }
             }
         };
-
-        return res.status(200).json({ 
+            return res.status(200).json({ 
             success: true, 
             keys: keys 
         });
