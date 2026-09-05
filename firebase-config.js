0d99e4251a83a256897a5a5a69d694e1ddb477a7
BarboSSA <htutkhaungkoko336@gmail.com>
2026-09-05 16:22:40 +0630

    410

--- firebase-config.js
+++ firebase-config.js
@@ -1,6 +1,7 @@
 // firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
+import { getAuth, signInAnonymously } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBPwRCFuMrAlV544eBiWarQYBADA88Jr4",
@@ -14,3 +15,13 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+export const auth = getAuth(app);
+
+// App စဖွင့်တာနဲ့ Anonymous (ကိုယ်စားလှယ်) အနေနဲ့ အလိုအလျောက် Login ဝင်ထားစေရန်
+signInAnonymously(auth)
+  .then((userCredential) => {
+    console.log("Anonymous Auth Successful:", userCredential.user.uid);
+  })
+  .catch((error) => {
+    console.error("Auth Error:", error);
+  });
